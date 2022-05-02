2d2613784dc9ecd949a6910b263509a2ec42460b
Jonas Lang <jonas@alur.de>
2022-05-02 15:25:42 +0200

    Initial Commit

--- Package.swift
+++ Package.swift
@@ -5,6 +5,11 @@ import PackageDescription
 
 let package = Package(
     name: "ImportantUtilities",
+    platforms: [
+        .iOS(.v13),
+        .tvOS(.v13),
+        .watchOS(.v5),
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
