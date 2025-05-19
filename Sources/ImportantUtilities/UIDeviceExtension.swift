27cf32e085d3314b43ccee5f88fb35de18b23bbe
Jonas Lang <jonas@alur.de>
2025-05-19 23:09:06 +0200

    added new devices

--- Sources/ImportantUtilities/UIDeviceExtension.swift
+++ Sources/ImportantUtilities/UIDeviceExtension.swift
@@ -1,6 +1,6 @@
 //
 //  UIDeviceExtension.swift
-//  
+//
 //
 //  Created by Jonas Lang on 31.05.22.
 //
@@ -68,10 +68,15 @@ public extension UIDevice {
             case "iPhone14,8":                                      return "iPhone 14 Plus"
             case "iPhone15,2":                                      return "iPhone 14 Pro"
             case "iPhone15,3":                                      return "iPhone 14 Pro Max"
-			case "iPhone15,4":                                      return "iPhone 15"
-			case "iPhone15,5":                                      return"iPhone 15 Plus"
-			case "iPhone16,1":                                      return "iPhone 15 Pro"
-			case "iPhone16,2":                                      return "iPhone 15 Pro Max"
+            case "iPhone15,4":                                      return "iPhone 15"
+            case "iPhone15,5":                                      return"iPhone 15 Plus"
+            case "iPhone16,1":                                      return "iPhone 15 Pro"
+            case "iPhone16,2":                                      return "iPhone 15 Pro Max"
+            case "iPhone17,3":                                      return "iPhone 16"
+            case "iPhone17,4":                                      return "iPhone 16 Plus"
+            case "iPhone17,1":                                      return "iPhone 16 Pro "
+            case "iPhone17,2":                                      return "iPhone 16 Pro Max"
+            case "iPhone17,5":                                      return "iPhone 16e"
                 
             case "iPad1,1", "iPad1,2":                              return "iPad (1st generation)"
             case "iPad2,1", "iPad2,2", "iPad2,3", "iPad2,4":        return "iPad (2nd generation)"
@@ -82,17 +87,21 @@ public extension UIDevice {
             case "iPad7,11", "iPad7,12":                            return "iPad (7th generation)"
             case "iPad11,6", "iPad11,7":                            return "iPad (8th generation)"
             case "iPad12,1", "iPad12,2":                            return "iPad (9th generation)"
+            case "iPad13,18", "iPad13,19":                          return "iPad (10th generation)"
             case "iPad4,1", "iPad4,2", "iPad4,3":                   return "iPad Air (1st generation)"
             case "iPad5,3", "iPad5,4":                              return "iPad Air (2nd generation)"
             case "iPad11,3", "iPad11,4":                            return "iPad Air (3rd generation)"
             case "iPad13,1", "iPad13,2":                            return "iPad Air (4th generation)"
             case "iPad13,16", "iPad13,17":                          return "iPad Air (5th generation)"
+            case "iPad14,8", "iPad14,9":                            return "iPad Air (11-inch) (M2)"
+            case "iPad14,10", "iPad14,11":                          return "iPad Air (13-inch) (M2)"
             case "iPad2,5", "iPad2,6", "iPad2,7":                   return "iPad mini (1st generation)"
             case "iPad4,4", "iPad4,5", "iPad4,6":                   return "iPad mini (2nd generation)"
             case "iPad4,7", "iPad4,8", "iPad4,9":                   return "iPad mini (3rd generation)"
             case "iPad5,1", "iPad5,2":                              return "iPad mini (4th generation)"
             case "iPad11,1", "iPad11,2":                            return "iPad mini (5th generation)"
             case "iPad14,1", "iPad14,2":                            return "iPad mini (6th generation)"
+            case "iPad16,1", "iPad16,2":                            return "iPad mini (7th generation)"
             case "iPad6,3", "iPad6,4":                              return "iPad Pro (9.7-inch)"
             case "iPad7,3", "iPad7,4":                              return "iPad Pro (10.5-inch)"
             case "iPad8,1", "iPad8,2", "iPad8,3", "iPad8,4":        return "iPad Pro (11-inch) (3rd generation)"
@@ -103,41 +112,55 @@ public extension UIDevice {
             case "iPad8,5", "iPad8,6", "iPad8,7", "iPad8,8":        return "iPad Pro (12.9-inch) (3rd generation)"
             case "iPad8,11", "iPad8,12":                            return "iPad Pro (12.9-inch) (4th generation)"
             case "iPad13,8", "iPad13,9", "iPad13,10", "iPad13,11":  return "iPad Pro (12.9-inch) (5th generation)"
-				
-			case "AppleTV1,1":                                      return "Apple TV (1st generation)"
-			case "AppleTV2,1":                                      return "Apple TV (2nd generation)"
-			case "AppleTV3,1", "AppleTV3,2":                        return "Apple TV (3rd generation)"
-			case "AppleTV5,3":                                      return "Apple TV (4th generation)"
-			case "AppleTV6,2":                                      return "Apple TV 4K (1st generation)"
-			case "AppleTV11,1":                                     return "Apple TV 4K (2nd generation)"
+            case "iPad14,3", "iPad14,4":                            return "iPad Pro (11-inch) (6th generation)"
+            case "iPad14,5", "iPad14,6":                            return "iPad Pro (12.9-inch) (6th generation)"
+            case "iPad16,3", "iPad16,4":                            return "iPad Pro (11-inch) (M4) (7th generation)"
+            case "iPad16,5", "iPad16,6":                            return "iPad Pro (13-inch) (M4) (7th generation)"
+                
+            case "AppleTV1,1":                                      return "Apple TV (1st generation)"
+            case "AppleTV2,1":                                      return "Apple TV (2nd generation)"
+            case "AppleTV3,1", "AppleTV3,2":                        return "Apple TV (3rd generation)"
+            case "AppleTV5,3":                                      return "Apple TV (4th generation)"
+            case "AppleTV6,2":                                      return "Apple TV 4K (1st generation)"
+            case "AppleTV11,1":                                     return "Apple TV 4K (2nd generation)"
+            case "AppleTV14,1":                                     return "Apple TV 4K (3nd generation)"
             case "AudioAccessory1,1":                               return "HomePod"
             case "AudioAccessory5,1":                               return "HomePod mini"
+            case "AudioAccessory6,1":                               return "HomePod (2nd generation)"
             case "i386", "x86_64", "arm64":                         return "Simulator \(mapToDevice(identifier: ProcessInfo().environment["SIMULATOR_MODEL_IDENTIFIER"] ?? "iOS"))"
             default:                                                return identifier
             }
 #elseif os(tvOS)
             switch identifier {
-			case "AppleTV1,1":                                      return "Apple TV (1st generation)"
-			case "AppleTV2,1":                                      return "Apple TV (2nd generation)"
-			case "AppleTV3,1", "AppleTV3,2":                        return "Apple TV (3rd generation)"
-			case "AppleTV5,3":                                      return "Apple TV (4th generation)"
-			case "AppleTV6,2":                                      return "Apple TV 4K (1st generation)"
-			case "AppleTV11,1":                                     return "Apple TV 4K (2nd generation)"
+            case "AppleTV1,1":                                      return "Apple TV (1st generation)"
+            case "AppleTV2,1":                                      return "Apple TV (2nd generation)"
+            case "AppleTV3,1", "AppleTV3,2":                        return "Apple TV (3rd generation)"
+            case "AppleTV5,3":                                      return "Apple TV (4th generation)"
+            case "AppleTV6,2":                                      return "Apple TV 4K (1st generation)"
+            case "AppleTV11,1":                                     return "Apple TV 4K (2nd generation)"
+            case "AppleTV14,1":                                     return "Apple TV 4K (3nd generation)"
             case "i386", "x86_64": return "Simulator \(mapToDevice(identifier: ProcessInfo().environment["SIMULATOR_MODEL_IDENTIFIER"] ?? "tvOS"))"
             default: return identifier
             }
 #elseif os(watchOS)
-			switch identifier {
-			case "Watch1,1", "Watch1,2":                            return "Apple Watch"
-			case "Watch2,6", "Watch2,7":                            return "Apple Watch Series 1"
-			case "Watch2,3", "Watch2,4":                            return "Apple Watch Series 2"
-			case "Watch3,1", "Watch3,2", "Watch3,3", "Watch3,4":    return "Apple Watch Series 3"
-			case "Watch4,1", "Watch4,2", "Watch4,3", "Watch4,4":    return "Apple Watch Series 4"
-			case "Watch5,1", "Watch5,2", "Watch5,3", "Watch5,4":    return "Apple Watch Series 5"
-			case "Watch5,9", "Watch5,10", "Watch5,11", "Watch5,12": return "Apple Watch SE"
-			case "Watch6,1", "Watch6,2", "Watch6,3", "Watch6,4":    return "Apple Watch Series 6"
-			case "Watch6,6", "Watch6,7", "Watch6,8", "Watch6,9":    return "Apple Watch Series 7"
-			}
+            switch identifier {
+            case "Watch1,1", "Watch1,2":                            return "Apple Watch"
+            case "Watch2,6", "Watch2,7":                            return "Apple Watch Series 1"
+            case "Watch2,3", "Watch2,4":                            return "Apple Watch Series 2"
+            case "Watch3,1", "Watch3,2", "Watch3,3", "Watch3,4":    return "Apple Watch Series 3"
+            case "Watch4,1", "Watch4,2", "Watch4,3", "Watch4,4":    return "Apple Watch Series 4"
+            case "Watch5,1", "Watch5,2", "Watch5,3", "Watch5,4":    return "Apple Watch Series 5"
+            case "Watch5,9", "Watch5,10", "Watch5,11", "Watch5,12": return "Apple Watch SE"
+            case "Watch6,1", "Watch6,2", "Watch6,3", "Watch6,4":    return "Apple Watch Series 6"
+            case "Watch6,6", "Watch6,7", "Watch6,8", "Watch6,9":    return "Apple Watch Series 7"
+            case "Watch6,14", "Watch6,15", "Watch6,16", "Watch6,17":return "Apple Watch Series 8"
+            case "Watch6,10", "Watch6,11", "Watch6,12", "Watch6,13":return "Apple Watch SE (2nd generation)"
+            case "Watch6,18":										return "Apple Watch Ultra"
+            case "Watch7,1", "Watch7,2", "Watch7,3", "Watch7,4":    return "Apple Watch Series 9"
+            case "Watch7,5":										return "Apple Watch Ultra 2"
+            case "Watch7,8", "Watch7,9", "Watch7,10", "Watch7,11":  return "Apple Watch Series 10"
+                
+            }
 #elseif os(macOS)
             print("Mac")
             return "Mac"
@@ -170,15 +193,15 @@ public extension UIDevice {
     static var platform: Platform {
 #if os(iOS)
         let modelName = modelName.replacingOccurrences(of: "Simulator ", with: "").split(separator: " ")[0]
-		if modelName == "iPhone" || modelName == "iPod" {
+        if modelName.contains("iPhone") || modelName.contains("iPod") {
             return Platform.iOS
-        } else if modelName == "iPad" {
+        } else if modelName.contains("iPad") {
             return Platform.iPadOS
         } else {
             return Platform.Undefined
         }
 #elseif os(watchOS)
-		return Platform.watchOS
+        return Platform.watchOS
 #elseif os(macOS)
         return Platform.macOS
 #else
@@ -191,7 +214,7 @@ public extension UIDevice {
 #if os(iOS)
         return Platform.iOS
 #elseif os(watchOS)
-		return Platform.watchOS
+        return Platform.watchOS
 #elseif os(macOS)
         return Platform.macOS
 #else
@@ -202,7 +225,7 @@ public extension UIDevice {
     enum Platform: String {
         case iOS
         case iPadOS
-		case watchOS
+        case watchOS
         case macOS
         case Undefined
     }
